586466dc6f6f9b699a0e725a56576d476df8bcd7
Richard <rngaboyisonga@gmail.com>
2023-06-20 17:29:59 +0200

    Connecting to the database

--- src/main/java/com/TodoSpring/TodoSpring/TaskController.java
+++ src/main/java/com/TodoSpring/TodoSpring/TaskController.java
@@ -4,11 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @Controller
 public class TaskController {
+
+    private final DataSource dataSource;
+
+    public TaskController(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @RequestMapping("/tasks")
     public ModelAndView home() {
         ModelAndView modelAndView = new ModelAndView("hometasks");
+
+        // Check database connection
+        try (Connection connection = dataSource.getConnection()) {
+            modelAndView.addObject("message", "Successfully connected to the database.");
+        } catch (SQLException e) {
+            modelAndView.addObject("message", "Failed to connect to the database.");
+        }
         return modelAndView;
     }
 }
